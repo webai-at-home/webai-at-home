8e427111609ec7c7aaf7c182ed82ed3c5ea62edb
Jerome Etienne <jerome.etienne@gmail.com>
2026-08-14 05:46:29 +0200

    Add a Worker link to the debug page's navigation bar
    Brings the debug page in line with the home, monitor, and ledger pages,
    using the same shared WorkerPageOrigin.wireLinks helper.

--- packages/gateway/web/debug/src/main.ts
+++ packages/gateway/web/debug/src/main.ts
@@ -1,3 +1,5 @@
 import { ThemeToggle } from '../../_shared/theme_toggle.js';
+import { WorkerPageOrigin } from '../../_shared/worker_page_origin.js';
 
 ThemeToggle.setup();
+WorkerPageOrigin.wireLinks(['#worker-link-nav']);
